f0a6511ba6b0465117c42544691f3f1fd6d7f60d
Aria-Qii <740497484@qq.com>
2026-08-19 16:38:07 +0800

    update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yanji-v14';
+const CACHE = 'yanji-v15';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
